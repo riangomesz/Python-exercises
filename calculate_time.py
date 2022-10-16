3b1abace58ef4b21e7900ac644f6d9e16b696eb6
Rian Gomes <80797632+riangomesz@users.noreply.github.com>
2022-10-15 22:48:34 -0300

    Update calculate_time.py

--- calculate_time.py
+++ calculate_time.py
@@ -1,3 +1,9 @@
+# Programa que recebe um horário estabelecido pelo usuário, 
+# E SOMA com os minutos também informado pelo mesmo.
+# Retornando o horário em que o evento acaba.
+
+
+
 hour = int(input("Hora inicial: "))
 mins = int(input("Minutos iniciais: "))
 dura = int(input("Duração do evento em minutos: "))
