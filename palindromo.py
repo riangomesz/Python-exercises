02e1697fbb86e38fbfed47388f95eb97c714cc0b
Rian Gomes <80797632+riangomesz@users.noreply.github.com>
2022-10-15 22:34:00 -0300

    Update palindromo.py

--- palindromo.py
+++ palindromo.py
@@ -1,3 +1,7 @@
+# Programa que recebe uma palavara, e diz se a palavra digitada forma um palindromo ou não.
+# Também retorna ao usuário a mesma escrita de trás pra frente e da forma normal.
+
+
 frase = str(input("Digite uma frase: ")).strip().upper()
 palavras = frase.split()
 print("Você digitou {} ".format(palavras))
