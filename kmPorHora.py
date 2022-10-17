6b6e77f3d02fabce2ffa5164911437dfe351d0d4
Rian Gomes <80797632+riangomesz@users.noreply.github.com>
2022-10-17 11:27:24 -0300

    Update kmPorHora.py

--- kmPorHora.py
+++ kmPorHora.py
@@ -1,3 +1,8 @@
+# Programa que calcula o tempo estimado em horas 
+# A partir da distância em km informada
+# E da velocidade média km/h
+
+
 distancia = float(input("Digite a distância em km:"))
 velocidade = float(input("Digite a velocidade média em km/h:"))
 
