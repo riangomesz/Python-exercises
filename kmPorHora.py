6b209ae63b1601f5f20742c49419328f7e37050c
Rian Gomes <80797632+riangomesz@users.noreply.github.com>
2022-10-17 11:25:16 -0300

    Add files via upload

---
+++ kmPorHora.py
@@ -0,0 +1,6 @@
+distancia = float(input("Digite a distância em km:"))
+velocidade = float(input("Digite a velocidade média em km/h:"))
+
+tempo = distancia/velocidade
+
+print("O tempo estimado é de %5.2f horas" % tempo)
