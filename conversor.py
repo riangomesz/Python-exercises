9b614652eed9d12d32f6e6c42fa72b6a2cb01484
Rian Gomes <80797632+riangomesz@users.noreply.github.com>
2022-10-17 10:43:43 -0300

    Update conversor.py

--- conversor.py
+++ conversor.py
@@ -1,3 +1,6 @@
+# Programa que recebe um número de dias, horas e minutos.
+# E calcula para o usuário a conversão dos valores informados para minutos
+
 
 dias = int(input("Dias: "))
 horas = int(input("Horas: "))
