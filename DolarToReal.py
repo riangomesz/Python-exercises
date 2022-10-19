5c7c75a1e616f0449e8747df0025c0dc5c4420b0
Rian Gomes <80797632+riangomesz@users.noreply.github.com>
2022-10-19 12:48:09 -0300

    Add files via upload

---
+++ DolarToReal.py
@@ -0,0 +1,11 @@
+print("Conversor de Dólar para Real")
+
+
+qtde_dolar = float(input("\nUSD:"))
+qtde_real = float(input("R$:"))
+
+dolar_to_real = qtde_dolar * 5.29
+real_to_dolar = qtde_real * 0.19
+
+print("$",qtde_dolar,"dolars é",round(dolar_to_real, 2),"reais")
+print("R$",qtde_real,"reais é",round(real_to_dolar, 2),"dolars")
