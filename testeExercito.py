407f2d91f725895663942e21cba7644ba10390d4
Rian Gomes <80797632+riangomesz@users.noreply.github.com>
2022-10-19 16:13:59 -0300

    Add files via upload

---
+++ testeExercito.py
@@ -0,0 +1,10 @@
+idade = int(input())
+nome = str(input())
+altura = float(input())
+peso = float(input())
+
+if (idade >= 18 and peso >= 60 and altura >= 1.70):
+     print("Você está apto a servir o exercito")
+else:
+     print ("Você não está apto a servir o exercito")
+
