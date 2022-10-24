a6f04fa553c27097f3962012919a658f0b0d2e0e
Rian Gomes <80797632+riangomesz@users.noreply.github.com>
2022-10-24 10:57:18 -0300

    Add files via upload

---
+++ eleicao_Brasil_2022.py
@@ -0,0 +1,15 @@
+print("Eleições Brasil 2022")
+print("Digite \n - Para votar no Bolsonaro do Partido Liberal aperte 22\n - Para votar no Lula do Partido dos Trabalhadores aperte 13 \n - Aperte 0, caso queire votar Nulo")
+
+for  i in range(2):
+    nome = str(input("\n Informe seu Nome:"))
+    voto = int(input(" Informe seu voto:"))
+    if voto == 22:
+        print("Você votou no Bolsonaro.")
+    elif voto == 13:
+        print("Você votou no Lula.")
+    elif voto == 0:
+        print("Você anulou o seu voto")
+    else:
+        print("Digite algum dos números dos candidatos para votar.")
+
